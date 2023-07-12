cb2d8373836d6063a36ae65882d431a5ca476f5a
Jonathan Lorenzo <crossvampirene0@gmail.com>
2023-07-12 16:09:48 +0800

    Chat Model Firebase Integration

--- src/models/chat.js
+++ src/models/chat.js
@@ -1,43 +1,46 @@
-import { addDoc, collection } from "firebase/firestore";
-import { db } from "src/boot/firebaseConnection";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  where,
+  or
+} from "firebase/firestore";
+import { db } from 'boot/firebaseConnection'
 
-const allChats = () => {
-  return JSON.parse(localStorage.getItem("chats") || "[]");
+const allChats = async () => {
+  const q = query(collection(db, "chats"),
+    or(where('sender_id', '==', '1234'),
+      where('receiver_id', '==', '1234')
+    )
+  );
+  const querySnapshot = await getDocs(q);
+
+  let chats = [];
+  querySnapshot.forEach((doc) => {
+    chats.push({ ...doc.data(), id: doc.id });
+  });
+
+  return chats;
 };
 
-export const getChats = (user_id) => {
-  return new Promise((resolve) => {
-    setTimeout(function () {
-      const chats = allChats();
+export const getChats = async (user_id) => {
+  const q = query(collection(db, "chats"),
+    or(where('sender_id', '==', user_id),
+      where('receiver_id', '==', user_id)
+    )
+  );
+  const querySnapshot = await getDocs(q);
 
-      resolve(
-        chats.filter(
-          (chat) => chat.sender_id === user_id || chat.receiver_id === user_id
-        )
-      );
-    }, 1000);
+  let chats = [];
+  querySnapshot.forEach((doc) => {
+    chats.push({ ...doc.data(), id: doc.id });
   });
+
+  return chats;
 };
 
 export const createChat = async (text, sender, receiver) => {
-  // return new Promise((resolve) => {
-  //   setTimeout(async () => {
-  //     let chats = allChats();
-  //     let data = {
-  //       text,
-  //       sender_id: sender.id,
-  //       receiver_id: receiver.id,
-  //       sender_name: sender.name,
-  //       receiver_name: receiver.name,
-  //       id: chats.length + 1,
-  //     };
-
-  //     chats.push(data);
-  //     localStorage.setItem("chats", JSON.stringify(chats));
-
-  //     resolve(data);
-  //   }, 1000);
-  // });
   let data = {
     text,
     sender_id: sender.id,
@@ -45,6 +48,8 @@ export const createChat = async (text, sender, receiver) => {
     sender_name: sender.name,
     receiver_name: receiver.name
   };
+
   const docRef = await addDoc(collection(db, "chats"), data);
-  return docRef.id
+
+  return docRef.id;
 };
